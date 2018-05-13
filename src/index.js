e86f8c8380a9d26f1ea89d369c74f394af044f3e
koobohyeon <goosee115@gmail.com>
2018-05-14 02:13:45 +0900

    change logic

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 //오목
 class gomoku {
-  board = this.Create2DArray(15,15);
-  player = 'black';
+  board = this.Create2DArray(15, 15);
+  player = "black";
   row = 15;
   col = 15;
   Create2DArray(rows, columns) {
@@ -12,80 +12,60 @@ class gomoku {
     return x;
   }
   init() {
-    this.Create2DArray(this.col,this.row);
-    this.player = 'black';
+    this.Create2DArray(this.col, this.row);
+    this.player = "black";
   }
-  isFiveStones(black, white) {
-    if (black === 5) {
-      return 'black'
-    } else if (white === 5) {
-      return 'white'
-    }
-  }
-  // 같은 행 확인 (가로)
-  checkCol() {
-    for (let i = 0; i < this.col; i++){
-      const black = 0;
-      const white = 0;
-      for(let j = 0; j < this.row; j++){
-        if(this.board[i][j] === undefined) {
-          black = 0;
-          white = 0;
-        } else if (this.board[i][j] === 'black') {
-          white = 0;
-          black++;
-        } else {
-          black = 0;
-          white ++
-        } 
-        const fiveStones = this.isFiveStones(black, white);
-        if(fiveStones){
-          return fiveStones;
-        }
-      }
-    }
-  }
-  // 같은 열 확인(세로)
-  checkRow() {
-    for (let i = 0; i < this.col; i++){
-      const black = 0;
-      const white = 0;
-      for(let j = 0; j < this.row; j++){
-        if(this.board[j][i] === undefined) {
-          black = 0;
-          white = 0;
-        } else if (this.board[j][i] === 'black') {
-          white = 0;
-          black++;
-        } else {
-          black = 0;
-          white ++
-        } 
-        const fiveStones = this.isFiveStones(black, white);
-        if(fiveStones){
-          return fiveStones;
-        }
-      }
-    }
-  }
-  // 대각선 확인
-  checkDiagonal() {
-    for(let i = 0; i < 10; i++) {
-      const black = 0;
-      const white = 0;
-      for (let j = 0; i + j < this.col; j++){
-      if (this.board[i][i] === undefined) {
+  // 풀어놓은 배열에 돌이 다섯 개인지 확인한다.
+  isFiveStones(arr) {
+    const white = 0;
+    const black = 0;
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] === undefined) {
+        white = 0;
         black = 0;
+        continue;
+      } else if (arr[i] === "black") {
+        black++;
         white = 0;
-      } else if (this.board[j][j+i] === 'black') {
-        white = 0;
-        black++
       } else {
+        white++;
         black = 0;
-        white++
       }
-     }
+    }
+    if (black === 5) {
+      return "black";
+    } else if (white === 5) {
+      return "white";
     }
   }
-  
+  // 클릭된 좌표를 넣어서 배열로 풀기
+  toOneDimensionalArr(col, row) {
+    const colArr = [];
+    const rowArr = [];
+    const toPlusDiagonalArr = [];
+    const toMinusDiagonalArr = [];
+    for (let i = 0; i < this.col; i++) {
+      // 가로 세로 방향 배열로 넣기 
+      colArr.push(this.board[col][i]);
+      rowArr.push(this.board[i][row]);
+      // 대각선방향 
+      //row 가 클 때
+      if (col < row) {
+        if(this.board[i][(row - col) + i] != undefined){
+          toPlusDiagonalArr.push(this.board[i][(row - col) + i]);
+        }
+        if(this.board[row + col - i][i] != undefined){
+          toMinusDiagonalArr.push(this.board[row + col - i][i])
+        }
+        // col이 클 때 
+      } else {
+        if(this.board[(col - row) + i][i]) {
+          toPlusDiagonalArr.push(this.board[(col - row) + i][i]);
+        }
+        if(this.board[14 - i][row - (14 - col) + i]) {
+          toMinusDiagonalArr.push(this.board[14 - i][row - (14 - col) + i])
+        }
+      }
+    }
+  }
 }
